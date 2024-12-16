6f8297c04cc783080e5f98c6d1a0c009c090117c
lianecagara <nealiana.kaye.cagara@gmail.com>
2024-12-16 15:01:43 +0800

    New api

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 // Dont use DOMContentLoaded listener.
 // you shouldnt do dom outside these functions
 
+const { FileHandler } = window;
+
+const fileHandler = new FileHandler();
+
 export const extraMenu = [
   {
     label: "Option 1",
@@ -19,3 +23,117 @@ export const extraMenu = [
 export function firstOpen() {}
 
 export function everyOpen() {}
+
+export const menuBarClass = ["lia-menubar", "small"];
+
+export const menuBar = [
+  {
+    name: "File",
+    data: [
+      {
+        label: "New",
+        callback(event) {},
+      },
+      {
+        label: "Open",
+        async callback(event) {
+          const file = await fileHandler.open(".txt");
+          const data = await fileHandler.readFile(file, "text");
+          //
+        },
+      },
+      {
+        label: "Download",
+        async callback(event) {
+          const text = "example";
+          await fileHandler.save(text, "example.txt");
+          //
+        },
+      },
+    ],
+  },
+  {
+    name: "Edit",
+    data: [
+      {
+        label: "Option",
+        callback() {},
+      },
+      {
+        label: "Option",
+        callback() {},
+      },
+      {
+        label: "Option",
+        callback() {},
+      },
+      {
+        label: "Option",
+        callback() {},
+      },
+    ],
+  },
+  {
+    name: "Option",
+    data: [
+      {
+        label: "Option",
+        callback() {},
+      },
+      {
+        label: "Option",
+        callback() {},
+      },
+      {
+        label: "Option",
+        callback() {},
+      },
+      {
+        label: "Option",
+        callback() {},
+      },
+    ],
+  },
+  {
+    name: "Option",
+    data: [
+      {
+        label: "Option",
+        callback() {},
+      },
+      {
+        label: "Option",
+        callback() {},
+      },
+      {
+        label: "Option",
+        callback() {},
+      },
+      {
+        label: "Option",
+        callback() {},
+      },
+    ],
+  },
+  {
+    name: "Option",
+    data: [
+      {
+        label: "Option",
+        callback() {},
+      },
+      {
+        label: "Option",
+        callback() {},
+      },
+      {
+        label: "Option",
+        callback() {},
+      },
+      {
+        label: "Option",
+        callback() {},
+      },
+    ],
+  },
+];
